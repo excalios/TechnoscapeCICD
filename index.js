9d1f71701eaa6a3669fbe54b38ed138dcf7f3370
Ravi Deevan <ravideevan3@gmail.com>
2021-06-24 19:46:59 +0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const Products = require('./product.json');
+
+app.get('/', (req, res) => {
+	res.send('simple rest app');
+});
+
+app.get('/product/listall', (req, res) => {
+	res.json(Products);
+});
+
+app.get('/product/:id', (req, res) => {
+	res.json(
+		Products.find((product) => {
+			return req.params.id == product.id;
+		})
+	);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
