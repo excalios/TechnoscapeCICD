e858890f58a2a8f3b1d2c695c8d22adbc4a554da
Ravi Deevan <ravideevan3@gmail.com>
2021-06-25 21:36:25 +0800

    Day 2

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ app.get('/product/:id', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
+
+module.exports = server;
