f193a83c03ec390ab32bf9b8b88e91a415ee7150
Ravi Deevan <ravideevan3@gmail.com>
2021-07-06 13:27:23 +0800

    change indents

--- index.js
+++ index.js
@@ -3,25 +3,31 @@ const app = express();
 const Products = require('./product.json');
 
 app.get('/', (req, res) => {
-	res.send('simple rest app');
+  res.send('simple rest app');
 });
 
 app.get('/product/listall', (req, res) => {
-	res.json(Products);
+  res.json(Products);
 });
 
 app.get('/product/:id', (req, res) => {
-	res.json(
-		Products.find((product) => {
-			return req.params.id == product.id;
-		})
-	);
+  res.json(
+    Products.find((product) => {
+      return req.params.id == product.id;
+    })
+  );
+  console.log('hello world');
+  if (true) {
+    if (false) {
+      console.log();
+    }
+  }
 });
 
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = server;
